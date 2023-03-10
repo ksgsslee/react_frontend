99a5a7f353c64b6289cc660e2a9abfe51bfda5c5
ksgsslee <71924123+ksgsslee@users.noreply.github.com>
2023-03-11 00:31:32 +0900

    팔로잉 기능

--- src/components/SuggestionList.js
+++ src/components/SuggestionList.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './SuggestionList.scss';
 import { Card } from 'antd';
 import Suggestion from './Suggestion';
 import { useAppContext } from 'store';
 import useAxios from 'axios-hooks';
+import axios from 'axios';
 
 export default function SuggestionList({ style }) {
   const {
@@ -11,10 +12,32 @@ export default function SuggestionList({ style }) {
   } = useAppContext();
   const headers = { Authorization: `JWT ${jwtToken}` };
 
-  const [{ data: userList, loading, error }, refetch] = useAxios({
+  const [{ data: oriUserList, loading, error }, refetch] = useAxios({
     url: 'http://127.0.0.1:8000/accounts/suggestions/',
     headers,
   });
+  const [userList, setUserList] = useState([]);
+
+  useEffect(() => {
+    if (!oriUserList) setUserList([]);
+    else
+      setUserList(oriUserList.map((user) => ({ ...user, is_follow: false })));
+  }, [oriUserList]);
+
+  const onFollowUser = (username) => {
+    axios
+      .post('http://127.0.0.1:8000/accounts/follow/', { username }, { headers })
+      .then((response) => {
+        setUserList((prevUserList) =>
+          prevUserList.map((user) =>
+            user.username !== username ? user : { ...user, is_follow: true },
+          ),
+        );
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
   return (
     <div style={style}>
@@ -22,13 +45,13 @@ export default function SuggestionList({ style }) {
       {error && <div>로딩 중 에러가 발생했습니다.</div>}
       <button onClick={refetch}>Reload</button>
       <Card title="Suggestion For You" size="small">
-        {userList &&
-          userList.map((suggestionUser, index) => (
-            <Suggestion
-              key={suggestionUser.username}
-              suggestionUser={suggestionUser}
-            />
-          ))}
+        {userList.map((suggestionUser, index) => (
+          <Suggestion
+            key={suggestionUser.username}
+            suggestionUser={suggestionUser}
+            onFollowUser={onFollowUser}
+          />
+        ))}
       </Card>
     </div>
   );
